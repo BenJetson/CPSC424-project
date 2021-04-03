b713587face71763f1bac113ef1ffceefd4da114
Avery Holder <holder9@clemson.edu>
2021-04-03 00:34:24 -0400

    Did some testing in test.py and changes to the gui are reflected in gui.py. GUI is created dynamically.

--- src/gui.py
+++ src/gui.py
@@ -1,41 +1,33 @@
+from defaults import generate
 import tkinter as tk                    
 from tkinter import ttk
-  
-  
+
+# Create main window frame
 root = tk.Tk()
 root.title("Tab Widget")
-tabControl = ttk.Notebook(root, width=600, height=300)
-  
+tabControl = ttk.Notebook(root)
+
 security_tab = ttk.Frame(master=tabControl)
 misc_tab = ttk.Frame(master=tabControl)
 
-vals = ('5','10','15','20','25','30')
-  
-tabControl.add(security_tab, text ='Security')
-tabControl.add(misc_tab, text ='Miscellanious')
-tabControl.pack(expand = 1, fill ="both")
+# Generate the list of settings
+mgr = generate()
 
-#Stuff for the security tab. Labels, plus a text entry.
-label1 = tk.Label(master=security_tab, text="Screen Timeout").grid(row=0, column=0)
-entry1 = tk.Entry(master=security_tab).grid(row=0, column=1)
+# Iterate over each group and create a tab for the group.
+for group in mgr.groups:
+    tab = ttk.Frame(master=tabControl)
+    tabControl.add(tab, text=group.title)
+    tabControl.pack(expand=1, fill="both")
 
-# Stuff to create a combobox/dropdown selector
-label4 = tk.Label(master=security_tab, text="Screen Timeout").grid(row=1, column=0)
-n = tk.StringVar(value="5")
-combobox = ttk.Combobox(master=security_tab, values=vals, textvariable=n).grid(row=1, column=1, pady=15)
-
-# Create a radio button
-label5 = tk.Label(master=security_tab, text="Screen Timeout").grid(row=2, column=0)
-radiobutton_t = tk.Radiobutton(master=security_tab, text="True", value="true").grid(row=2, column=1)
-radiobutton_f = tk.Radiobutton(master=security_tab, text="False", value="false").grid(row=2, column=2)
-
-# Misc tab stuff
-label2 = tk.Label(master=misc_tab, text="This is the second tab")
-label2.pack()
-  
+    # Iterate over each setting in the group
+    # Add a label and control for each
+    for setting in group.items:
+        setting_frame = ttk.Frame(master=tab)
+        label = tk.Label(master=setting_frame, text=setting.name, width=30, anchor="w").grid(row=0, column=0, padx=5, pady=5)
+        entry = tk.Entry(master=setting_frame, width=20).grid(row=0, column=1, padx=5)
+        setting_frame.pack()
 
 
-B = tk.Button(text="Apply")
-B.pack(side=tk.RIGHT)
+B = tk.Button(text="Apply").pack(side=tk.RIGHT, padx=5, pady=5)
   
 root.mainloop() 
--- src/test.py
+++ src/test.py
@@ -1,11 +1,36 @@
 from defaults import generate
+import tkinter as tk                    
+from tkinter import ttk
+
+# Create main window frame
+root = tk.Tk()
+root.title("Tab Widget")
+tabControl = ttk.Notebook(root)
+
+security_tab = ttk.Frame(master=tabControl)
+misc_tab = ttk.Frame(master=tabControl)
+
+
 
 mgr = generate()
 
 for group in mgr.groups:
     print(f"Group: {group.title}")
+    tab = ttk.Frame(master=tabControl)
+    tabControl.add(tab, text=group.title)
+    tabControl.pack(expand=1, fill="both")
 
     for setting in group.items:
         print(f"    Setting: {setting.name} has value {setting.get_value()} and is type {setting.get_kind()}")
+        setting_frame = ttk.Frame(master=tab)
+        label = tk.Label(master=setting_frame, text=setting.name, width=30, anchor="w").grid(row=0, column=0, padx=5, pady=5)
+        entry = tk.Entry(master=setting_frame, width=20).grid(row=0, column=1, padx=5)
+        setting_frame.pack()
 
     print("")
+
+
+B = tk.Button(text="Apply")
+B.pack(side=tk.RIGHT)
+  
+root.mainloop() 
