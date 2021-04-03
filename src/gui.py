561a16598839f2ef807c8560605fe9f9ac5b3d3f
Avery Holder <holder9@clemson.edu>
2021-04-03 01:48:26 -0400

    Added a list of lists for the settings. settings_list is a list of temp_lists. Each temp_list has the name of the setting and the reference to the entry for that setting. I defined a function called get_vals() that iterates over the lists and uses the reference to the entry to get the value in that box and prints to console. The apply button is set to call that function.

--- src/gui.py
+++ src/gui.py
@@ -3,6 +3,12 @@ from base import SettingType
 import tkinter as tk                    
 from tkinter import ttk
 
+# Define function for apply button
+def get_vals():
+    print("Setting values:")
+    for setting_obj in settings_list:
+        print("    " + setting_obj[0] + ": " + setting_obj[1].get())
+
 # Create main window frame
 root = tk.Tk()
 root.title("Tab Widget")
@@ -10,6 +16,9 @@ tabControl = ttk.Notebook(root)
 
 security_tab = ttk.Frame(master=tabControl)
 misc_tab = ttk.Frame(master=tabControl)
+
+# List of settings and values. This will be a list of lists.
+settings_list = []
 
 # Generate the list of settings
 mgr = generate()
@@ -26,12 +35,18 @@ for group in mgr.groups:
         setting_frame = ttk.Frame(master=tab)
         label = tk.Label(master=setting_frame, text=setting.name, width=30, anchor="w").grid(row=0, column=0, padx=5, pady=5)
         if setting.get_kind() is SettingType.CHOICE:
-            entry = ttk.Combobox(master=setting_frame, values=setting.value_list, width=20).grid(row=0, column=1, padx=5)
+            entry = ttk.Combobox(master=setting_frame, values=setting.value_list, width=20)
+            entry.grid(row=0, column=1, padx=5)
         else:
-            entry = tk.Entry(master=setting_frame, width=20).grid(row=0, column=1, padx=5)
+            entry = tk.Entry(master=setting_frame, width=20)
+            entry.grid(row=0, column=1, padx=5)
+
+        #add entry to settings_list
+        temp_list = [setting.name, entry]
+        settings_list.append(temp_list)
         setting_frame.pack()
 
 
-B = tk.Button(text="Apply").pack(side=tk.RIGHT, padx=5, pady=5)
+B = tk.Button(text="Apply", command=get_vals).pack(side=tk.RIGHT, padx=5, pady=5)
   
 root.mainloop() 
