4d991f0a01efe6aa4db7d8142350c8c9efd4978a
Avery Holder <holder9@clemson.edu>
2021-04-02 22:57:18 -0400

    Changed test file to reflect setting.get_kind()

--- src/test.py
+++ src/test.py
@@ -6,6 +6,6 @@ for group in mgr.groups:
     print(f"Group: {group.title}")
 
     for setting in group.items:
-        print(f"    Setting: {setting.name} has value {setting.get_value()}")
+        print(f"    Setting: {setting.name} has value {setting.get_value()} and is type {setting.get_kind()}")
 
     print("")
