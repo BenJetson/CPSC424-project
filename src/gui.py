9840a101875d8015d13fd150e78ce03eb5d32e7a
Ben Godfrey <BenJetson@users.noreply.github.com>
2021-04-03 12:43:54 -0400

    Use custom widget for each Setting.

--- src/gui.py
+++ src/gui.py
@@ -1,21 +1,13 @@
 from defaults import generate
 from base import SettingType
-import tkinter as tk                    
+from gui_parts import SettingWidget
+import tkinter as tk
 from tkinter import ttk
-
-# Define function for apply button
-def get_vals():
-    print("Setting values:")
-    for setting_obj in settings_list:
-        print("    " + setting_obj[0] + ": " + setting_obj[1].get())
 
 # Create main window frame
 root = tk.Tk()
 root.title("Tab Widget")
 tabControl = ttk.Notebook(root)
-
-# List of settings and values. This will be a list of lists.
-settings_list = []
 
 # Generate the list of settings
 mgr = generate()
@@ -29,24 +21,12 @@ for group in mgr.groups:
     # Iterate over each setting in the group
     # Add a label and control for each
     for setting in group.items:
-        setting_frame = ttk.Frame(master=tab)
-        label = tk.Label(master=setting_frame, text=setting.name, width=30, anchor="w").grid(row=0, column=0, padx=5, pady=5)
-
-        # Set the input method based on setting type.
-        if setting.get_kind() is SettingType.CHOICE:
-            entry = ttk.Combobox(master=setting_frame, values=setting.value_list, width=20)
-            entry.grid(row=0, column=1, padx=5)
-        else:
-            entry = tk.Entry(master=setting_frame, width=20)
-            entry.grid(row=0, column=1, padx=5)
-
-        #add entry to settings_list
-        temp_list = [setting.name, entry]
-        settings_list.append(temp_list)
-        setting_frame.pack()
+        SettingWidget(setting, tab)
 
 # Button for applying settings
-B = tk.Button(text="Apply", command=get_vals).pack(side=tk.RIGHT, padx=5, pady=5)
-  
+B = tk.Button(text="Apply", command=mgr.save_to_disk).pack(
+    side=tk.RIGHT, padx=5, pady=5
+)
+
 # This shows the window and needs to be at the bottom iirc.
-root.mainloop() 
+root.mainloop()
---
+++ src/gui_parts.py
@@ -0,0 +1,112 @@
+#
+# CPSC 424-001 SP21
+# Term Project
+#
+# Ben Godfrey (bfgodfr)
+# and
+# Avery Holder (holder9)
+#
+# gui_parts.py - custom GUI widgets for our program.
+#
+
+from typing import Union
+import tkinter as tk
+from tkinter import ttk
+
+from base import Setting, SettingType
+
+
+class SettingWidget(ttk.Frame):
+    setting: Setting
+
+    widget_ckbx: ttk.Checkbutton
+
+    widget_entry: Union[ttk.Entry, ttk.Combobox]
+    entry_stringvar: tk.StringVar
+
+    def __init__(
+        self,
+        setting: Setting,
+        master: ttk.Frame,  # unlike ttk.Frame, this is required here.
+        **kwargs,
+    ) -> None:
+        # Pass necessary arguments to parent initializer.
+        super().__init__(master=master, **kwargs)
+
+        # Save a reference to the given Setting object.
+        self.setting = setting
+
+        # Construct widgets.
+        self.build_label()
+        self.build_ckbx()
+        self.build_entry()
+
+        # Pack widgets.
+        self.pack()
+
+    def build_label(self) -> None:
+        tk.Label(
+            master=self, text=self.setting.name, width=30, anchor="w"
+        ).grid(row=0, column=0, padx=5, pady=5)
+
+    def build_ckbx(self) -> None:
+        pass
+
+    def build_entry(self) -> None:
+        # Set the input method based on setting type.
+        if self.setting.get_kind() is SettingType.CHOICE:
+            self.widget_entry = ttk.Combobox(
+                master=self,
+                values=self.setting.value_list,  # list of legal values.
+                width=20,
+            )
+            self.widget_entry.bind(
+                "<<ComboboxSelected>>", self.handle_combobox_change
+            )
+        elif self.setting.get_kind() is SettingType.BOOLEAN:
+            self.widget_entry = ttk.Combobox(
+                master=self,
+                values=[True, False],  # for booleans, we shall show these.
+                width=20,
+                postcommand=self.handle_combobox_change,
+            )
+            self.widget_entry.bind(
+                "<<ComboboxSelected>>", self.handle_combobox_change
+            )
+        else:
+            self.entry_stringvar = tk.StringVar()
+            self.widget_entry = tk.Entry(
+                master=self,
+                width=20,
+                textvariable=self.entry_stringvar,
+                validate="all",
+                validatecommand=self.register(self.handle_entry_change),
+            )
+
+        self.widget_entry.grid(row=0, column=1, padx=5)
+
+    def handle_combobox_change(self, _: ttk.Combobox = None) -> None:
+        value = self.widget_entry.get()  # always a string.
+
+        # print(f"combobox change! got {value}")  # FIXME
+        self.set_from_string(value)
+
+    def handle_entry_change(self, _: ttk.Entry = None) -> bool:
+        value = self.entry_stringvar.get()  # always a string.
+
+        # print(f"entry change! got {value}")  # FIXME
+        self.set_from_string(value)
+
+        return True  # must return True for this to be called on each input.
+
+    def set_from_string(self, value: str) -> None:
+        if len(value) == 0:
+            self.setting.unset()
+            return
+
+        if self.setting.get_kind() == SettingType.NUMBER:
+            value = int(value)
+        if self.setting.get_kind() == SettingType.BOOLEAN:
+            value = True if value == "True" else False
+
+        self.setting.set_value(value)
